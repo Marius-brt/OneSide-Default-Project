591d8fe8b670aa368e020125764f3b6d27984e7f
Marius Brouty <marius.brouty@gmail.com>
2022-01-14 23:15:46 +0100

    v2.0.2

--- server.js
+++
@@ -1,9 +0,0 @@
-const oneside = require('oneside')
-
-oneside.get('/', (req, res) => {
-    oneside.send(res, "home.html", {
-        version: require('./package.json').dependencies.oneside.replace('^', '')
-    })
-})
-
-oneside.listen()
