75e19711efb60d44753ede4f480c17c71745de98
Marius Brouty <marius.brouty@gmail.com>
2022-01-15 01:36:52 +0100

    ts template

---
+++ index.ts
@@ -0,0 +1,17 @@
+import oneside from "oneside";
+
+const app = oneside.init({
+  showCompiling: true,
+  favicon: "favicon.ico",
+});
+
+app.get("/", (req: Request, res: Response) => {
+  oneside
+    .render("home", res)
+    .ejs({
+      version: require("./package.json").dependencies.oneside.replace("^", ""),
+    })
+    .send();
+});
+
+app.listen();
